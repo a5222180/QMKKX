fc195005087cc5223f1ff9484142957b6f737654
陈文俊 <462782495@qq.com>
2022-05-16 21:24:46 +0800

    Initial commit

---
+++ src/main/java/com/cwj/qmkkx/QmkkxApplication.java
@@ -0,0 +1,13 @@
+package com.cwj.qmkkx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmkkxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QmkkxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cwj/qmkkx/QmkkxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwj.qmkkx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmkkxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
